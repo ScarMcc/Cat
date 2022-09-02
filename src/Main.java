50a70c4ec889244a6d485fdbfe4ab4258eb89cf2
100164906 <scarmccloud@gmail.com>
2022-09-02 09:04:08 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|]---[|");
+        System.out.println("| o_o |");
+        System.out.println("[ _^_ ]");
+    }
+}
